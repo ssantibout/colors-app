d0cb3717c65af8aba617822ae6c99dc6cd2d638c
ssantibout <samuel.santibout@gmail.com>
2021-09-16 02:43:27 -0700

    Initial Commit Created cra and added seedPallets and pallet.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Pallet from './Pallet';
+import colorPallet from './seedPallet';
 import './App.css';
+import seedPallet from './seedPallet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pallet {...seedPallet[1]} />
     </div>
   );
 }
